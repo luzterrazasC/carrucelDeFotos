d1f01e2fb4ea57a39c05917f2a02cbd51cbf11da
Lucero Terrazas Cendejas <luzterra17@hotmail.com>
2020-07-07 14:19:25 -0500

    Initial Commit

---
+++ CarrucelFotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarrucelFotos
+//
+//  Created by Lucero Terrazas Cendejas on 7/7/20.
+//  Copyright © 2020 Erika Lucero Terrazas Cendejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

393009be1faf95e8ecc91a4332d5d12e762ae6fb
Lucero Terrazas Cendejas <luzterra17@hotmail.com>
2020-07-08 13:27:28 -0500

    cambio tamaño de imágenes

---
+++ CarrucelFotos/ImagesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesCollectionViewCell.swift
+//  CarrucelFotos
+//
+//  Created by Lucero Terrazas Cendejas on 7/8/20.
+//  Copyright © 2020 Erika Lucero Terrazas Cendejas. All rights reserved.
+//
+
+import UIKit
+
+class ImagesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imagePersonCell: UIImageView!
+}
--- CarrucelFotos/ViewController.swift
+++ CarrucelFotos/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+  
+    
 
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let arrayPersonImages = ["img1","img2","img3","img4", "img5", "img6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellCollection", for: indexPath) as! ImagesCollectionViewCell
+        
+        cell.imagePersonCell.image = UIImage(named: arrayPersonImages[indexPath.row])
+        return cell
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrayPersonImages.count
+    }
+    
+  
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let screen = UIScreen.main.bounds
+        
+        return CGSize(width: screen.width, height: screen.height)
+    }
+ 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.0
+    }
 }
 
